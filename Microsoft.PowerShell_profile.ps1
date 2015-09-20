9de058bccb1b543c375d636de0cfeb4f55cc98e0
Manish Gupta <manishkg97@gmail.com>
2015-09-19 22:41:12 -0400

    Powershell profile base

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+set-location c:
+set-alias python C:\Python32\python.exe -option ReadOnly
+set-alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe" -option ReadOnly
+set-alias pip "C:\Python32\Scripts\pip.exe" -option ReadOnly
+set-alias VBoxManage "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" -option ReadOnly
