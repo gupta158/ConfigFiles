128f2cf08bde7cc403ac01a8d3ee0e2738b8dcae
Manish Gupta <manishkg97@gmail.com>
2015-11-24 04:06:47 -0500

    Added another alias

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,5 @@ set-alias python C:\Python32\python.exe -option ReadOnly
 set-alias sublime "C:\Program Files\Sublime Text 3\sublime_text.exe" -option ReadOnly
 set-alias pip "C:\Python32\Scripts\pip.exe" -option ReadOnly
 set-alias VBoxManage "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" -option ReadOnly
+set-alias grepd Select-String 
+set-alias zip7 'C:\Program Files\7-Zip\7z.exe'
